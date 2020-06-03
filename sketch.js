4433b249ed9f4d8c5fc77f6145b0a806dd6b2f8f
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-06-03 15:43:09 +0300

    drawing the grid

--- sketch.js
+++ sketch.js
@@ -5,17 +5,38 @@ const settings = {
 };
 
 const sketch = () => {
+  // return the points on a 2D grid
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = x / (count - 1);
+        const v = y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid();
+  console.log(points);
+
   return ({ context, width, height }) => {
-    context.fillStyle = "orange";
+    context.fillStyle = "white";
+    // full size of the screen:
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
-    context.fillStyle = "red";
-    context.fill();
-    context.lineWidth = 40;
-    context.strokeStyle = "blue";
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 40;
+      context.stroke();
+    });
   };
 };
 
