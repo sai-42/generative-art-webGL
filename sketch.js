4b416dab30b301d22e7ac44df972e007e1ac9b08
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-06-10 11:33:58 +0300

    radius & organic randomness

--- sketch.js
+++ sketch.js
@@ -1,41 +1,49 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
   // return the points on a 2D grid
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 30;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          position: [u, v],
+        });
       }
     }
     return points;
   };
 
-  const points = createGrid();
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const { position, radius } = data;
+
+      const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
-      context.strokeStyle = "black";
-      context.lineWidth = 40;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = "red";
+      context.fill();
     });
   };
 };
