8240667240359a6c8edc9dfb7496dbec9478e1a8
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-06-24 14:34:18 +0300

    Shaders E.g: Step Functions

--- shader.js
+++ shader.js
@@ -25,18 +25,18 @@ const frag = glsl(/* glsl */`
     // center.x *= aspect;
 
   void main () {
-    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorA = sin(time) + vec3(1.0, 0.0, 0.0);
     vec3 colorB = vec3(0.0, 0.5, 0.0);
 
     vec2 center = vUv - 0.5;
-
     center.x *= aspect;
+
     float dist = length(center);
 
-    
+    float alpha = smoothstep(0.251, 0.25, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, dist > 0.25 ? 0.0: 1.0);    
+    vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    gl_FragColor = vec4(color, alpha);    
   }
 `);
 
@@ -50,6 +50,7 @@ const frag = glsl(/* glsl */`
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    clearColor: 'white',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
