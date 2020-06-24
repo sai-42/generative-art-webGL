720a204acfb2ede7e23228e095789544c61e53c3
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-06-24 14:53:19 +0300

    Shaders: Introducing Noise

--- shader.js
+++ shader.js
@@ -16,6 +16,8 @@ const frag = glsl(/* glsl */`
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   // main func - pixel manipulation
     // every shader has to have a main func
 
@@ -25,18 +27,23 @@ const frag = glsl(/* glsl */`
     // center.x *= aspect;
 
   void main () {
-    vec3 colorA = sin(time * 1.0) + vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 0.5, 0.0);
+    // vec3 colorA = sin(time * 1.0) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 0.5, 0.0);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
 
-    float dist = length(center);
+    // float dist = length(center);
 
-    float alpha = smoothstep(0.5, 0.05, dist);
+    // float alpha = smoothstep(0.5, 0.05, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
-    gl_FragColor = vec4(color, alpha);    
+    // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    // gl_FragColor = vec4(color, alpha);   
+
+  // noise func: pass in an xyz coordinate & we get back a number
+    float n = noise(vec3(center * 1.0, time));
+
+    gl_FragColor = vec4(vec3(n), 1.0);
   }
 `);
 
